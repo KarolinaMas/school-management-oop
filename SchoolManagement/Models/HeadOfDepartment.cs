de94b0a7cd3947419cac76de4de2816f5f060203
Karolina Maščinskaitė <kmkarole@yahoo.com>
2026-04-16 16:42:46 +0300

    add properties, override methods

---
+++ SchoolManagement/Models/HeadOfDepartment.cs
@@ -0,0 +1,29 @@
+namespace SchoolManagement.Models
+{
+    public class HeadOfDepartment : Teacher
+    {
+        public int TeamSize { get; set; }
+        public string DepartmentVision { get; set; } = string.Empty;
+
+        public override string GetRole()
+        {
+            return "Head of department";
+        }
+
+        public override string GetProfile()
+        {
+            return $"{base.GetProfile()}\nTeam size: {TeamSize}\nDepartment vision: {DepartmentVision}";
+        }
+
+        public override string GetContactInfo()
+        {
+            return $"{base.GetContactInfo()}, Head of department";
+        }
+
+        public override decimal CalculateAnnualBonus()
+        {
+            var result = base.CalculateAnnualBonus();
+            return result + CalculateBonusAmount(15m);
+        }
+    }
+}
