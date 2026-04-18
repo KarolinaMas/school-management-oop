678d841225c4d196c3f4b0abfd6b27c67b611ebf
Karolina Maščinskaitė <kmkarole@yahoo.com>
2026-04-18 15:13:37 +0300

    create objects using constructors

--- SchoolManagement/Program.cs
+++ SchoolManagement/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SchoolManagement.Models;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var underStudent = new UndergraduateStudent(1, "John", "Doe", "Computer science");
+            var gradStudent = new GraduateStudent(1, "Jane", "Doe", "AI programming");
+            var teacher = new Teacher(3, "Stephen", "Hawking", "Physics");
+            var departmentHead = new HeadOfDepartment(1, "Petras", "Petraitis", "Mathematics", 3);
+            var admin = new Administrator(5, "Ona", "Onaite", "23B");
+        }
+    }
+}
